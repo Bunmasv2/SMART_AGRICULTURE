d1e8d716f0f2a11f982a9a2273cbe9424347a76f
bunmasv2 <thaibao5335@gmail.com>
2026-03-14 20:04:00 +0700

    feat: initialize project structure for smart farm system

---
+++ Server/farm-manager-api/src/main/java/com/smartfarm/api/FarmManagerApplication.java
@@ -0,0 +1,13 @@
+package com.smartfarm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmManagerApplication.class, args);
+	}
+
+}
