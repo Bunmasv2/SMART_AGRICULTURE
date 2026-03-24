a060fbdcc60169cc54293a47cff9f6502cdf254f
Huycode11 <phamvanhuyhuy2020@gmail.com>
2026-03-24 22:53:01 +0700

    feat: implement email notifications for low inventory and expiring items

--- Server/farm-manager-api/src/main/java/com/smartfarm/api/repository/InventoryItemRepository.java
+++ Server/farm-manager-api/src/main/java/com/smartfarm/api/repository/InventoryItemRepository.java
@@ -3,9 +3,14 @@ package com.smartfarm.api.repository;
 import com.smartfarm.api.entity.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 @Repository
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Integer> {
     List<InventoryItem> findByCategory(String category);
+
+    @Query("SELECT i FROM InventoryItem i WHERE i.minThreshold > 0 AND " +
+           "(SELECT SUM(b.quantity) FROM InventoryBatch b WHERE b.item = i) <= i.minThreshold")
+    List<InventoryItem> findLowStockItems();
 }
